7d3848281d79f38853c34475ee381ed85a3f4c69
franderg <fgranadosvega@gmail.com>
2018-10-28 00:11:13 -0600

    update image

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,84 +22,32 @@
 	// Vars.
 		var	$body = document.querySelector('body');
 
-	// Disable animations/transitions until everything's loaded.
-		$body.classList.add('is-loading');
-
 		window.addEventListener('load', function() {
 			window.setTimeout(function() {
-				$body.classList.remove('is-loading');
-			}, 100);
+		//		$body.classList.remove('is-loading');
+			}, 0);
 		});
 
 	// Slideshow Background.
 		(function() {
-
-			// Settings.
-				var settings = {
-
-					// Images (in the format of 'url': 'alignment').
-						images: {
-							'images/bb.jpg': 'center'
-						},
-
-					// Delay.
-						delay: 0
-
-				};
-
 			// Vars.
-				var	pos = 0, lastPos = 0,
-					$wrapper, $bgs = [], $bg,
-					k, v;
-
+				var	$wrapper, $bgs = [], $bg, k;
 			// Create BG wrapper, BGs.
 				$wrapper = document.createElement('div');
-					$wrapper.id = 'bg';
-					$body.appendChild($wrapper);
+				$wrapper.id = 'bg';
+				$body.appendChild($wrapper);
 
-				for (k in settings.images) {
-
-					// Create BG.
-						$bg = document.createElement('div');
-							$bg.style.backgroundImage = 'url("'images/bb.jpg'")';
-							$bg.style.backgroundPosition = center;
-							$wrapper.appendChild($bg);
-
-					// Add it to array.
-						$bgs.push($bg);
-
-				}
+				// Create BG.
+				$bg = document.createElement('div');
+				$bg.style.backgroundImage = 'url(images/bb.jpg)';
+				$bg.style.backgroundPosition = 'center';
+				$wrapper.appendChild($bg);
+				// Add it to array.
+				$bgs.push($bg);
 
 			// Main loop.
-				$bgs[pos].classList.add('visible');
-				$bgs[pos].classList.add('top');
-
-				// Bail if we only have a single BG or the client doesn't support transitions.
-					if ($bgs.length == 1
-					||	!canUse('transition'))
-						return;
-
-				window.setInterval(function() {
-
-					lastPos = pos;
-					pos++;
-
-					// Wrap to beginning if necessary.
-						if (pos >= $bgs.length)
-							pos = 0;
-
-					// Swap top images.
-						$bgs[lastPos].classList.remove('top');
-						$bgs[pos].classList.add('visible');
-						$bgs[pos].classList.add('top');
-
-					// Hide last image after a short delay.
-						window.setTimeout(function() {
-							$bgs[lastPos].classList.remove('visible');
-						}, settings.delay / 2);
-
-				}, settings.delay);
-
+				$bgs[0].classList.add('visible');
+				//$bgs[0].classList.add('top');
 		})();
 
 	// Signup Form.
